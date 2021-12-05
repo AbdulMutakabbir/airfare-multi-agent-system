4b0c238b2b1ee42d55edf57b9d29fc8324c84bbb
Abdul Mutakabbir <47092538+SammSum1@users.noreply.github.com>
2021-12-05 11:28:06 +0530

    init airplanes file

--- src/Airplane.py
+++ src/Airplane.py
@@ -1,18 +1,40 @@
+from math import pow
+from datetime import date, timedelta
+
+
 class Airplane:
+    PRICE_PER_KILOMETER = 0.325
+    MAX_BOOKING_START_DAYS = 60
+    DEMAND_COEFFICIENT = 3
+    SALE_COEFFICIENT = 2
+    capacity = 250
 
-    def __init__(self, identifier, capacity, price_economy_class, price_business_class, price_first_class):
+    def __init__(self, identifier, airline, start_airport, end_airport, distance):
         self.airplane_id = identifier
-        self.airplane_capacity = capacity
-        self.economy_class_price = price_economy_class
-        self.business_class_price = price_business_class
-        self.firs_class_price = price_first_class
+        self.airline = airline
+        self.start_airport = start_airport
+        self.end_airport = end_airport
+        self.travel_distance = distance
+        self.departure_datetime_list = [date.today() + timedelta(3),date.today() + timedelta(5)]
 
-    def increment_prices(self):
-        self.economy_class_price += 10
-        self.business_class_price += 50
-        self.firs_class_price += 100
+    def __str__(self):
+        return self.airline + " : " + self.start_airport + ">" + self.end_airport
 
-    def decrement_prices(self):
-        self.economy_class_price -= 10
-        self.business_class_price -= 50
-        self.firs_class_price -= 100
+    def get_price(self, today, departure_date, demand):
+        if isinstance(today, date) and isinstance(departure_date, date) and isinstance(demand, float):
+            if departure_date in self.departure_datetime_list:
+                days_before_departure = (departure_date - today).days
+                if days_before_departure >= 0:
+                    time = days_before_departure / self.MAX_BOOKING_START_DAYS
+                    price_inc = demand - pow((- pow((time - 1), self.SALE_COEFFICIENT) - demand),
+                                             self.DEMAND_COEFFICIENT)
+
+                    price = (self.PRICE_PER_KILOMETER * self.travel_distance)
+                    price += price_inc * price
+                    return price
+                else:
+                    raise Exception("Flight already departed")
+            else:
+                raise Exception("No fight for the the given date")
+        else:
+            raise Exception("Type miss match")
